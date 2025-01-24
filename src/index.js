5b9a06c54a129c3da4154e2416a7eb7f6eab7a23
nevermind <nevermind359@gmail.com>
2025-01-23 23:36:43 -0500

    Fix sidebar height and remove close-button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 const menuToggle = document.getElementById('menu-toggle');
 const sidebar = document.getElementById('sidebar');
-const menuClose = document.querySelector('.menu-close');
 
 
 menuToggle.addEventListener('click', () => {
   sidebar.classList.toggle('active');
 });
-
-menuClose.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-})
 
